2b635c08d7503fc4f0b103f4501359d0b06d153f
pyllyukko <pyllyukko@maimed.org>
2015-05-16 10:03:35 +0300

    Added download_gists.sh

---
+++ download_gists.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+wget -nv -O 04/04.txt			https://gist.githubusercontent.com/tqbf/3132713/raw/40da378d42026a0731ee1cd0b2bd50f66aabac5b/gistfile1.txt
+wget -nv -O 06-hamming_distance/06.txt	https://gist.githubusercontent.com/tqbf/3132752/raw/cecdb818e3ee4f5dda6f0847bfd90a83edb87e73/gistfile1.txt
+wget -nv -O 07/07.txt			https://gist.githubusercontent.com/tqbf/3132853/raw/c02ff8a08ccf872f4cd278396379f4bb1ef337d8/gistfile1.txt
+wget -nv -O 08/08.txt			https://gist.githubusercontent.com/tqbf/3132928/raw/6f74d4131d02dee3dd0766bd99a6b46c965491cc/gistfile1.txt
+wget -nv -O 10-cbc/10.txt		https://gist.githubusercontent.com/tqbf/3132976/raw/f0802a5bc9ffa2a69cd92c981438399d4ce1b8e4/gistfile1.txt
