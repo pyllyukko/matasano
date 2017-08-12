5d16ea4376f9e3acbacc09707e8b6de1443f0405
pyllyukko <pyllyukko@maimed.org>
2017-08-12 10:09:44 +0300

    Cleanups to 16.c

--- 16/16.c
+++ 16/16.c
@@ -4,9 +4,9 @@
 #include "../lib/hacking.h"
 
 char
-    ciphertext[1024],
-    key[16],
-    iv[16];
+  ciphertext[1024],
+  key[16],
+  iv[16];
 
 int first_function(char *userdata, char *ciphertext)
 {
@@ -31,12 +31,14 @@ int first_function(char *userdata, char *ciphertext)
 
   // clear userdata2
   memset(userdata2, 0, sizeof(userdata2));
+  // prepend
   strncpy(userdata2, (char *)"comment1=cooking%20MCs;userdata=", strlen("comment1=cooking%20MCs;userdata="));
+  // user input
   strncat(userdata2, userdata, strlen(userdata));
+  // append
   strncat(userdata2, (char *)";comment2=%20like%20a%20pound%20of%20bacon", strlen(";comment2=%20like%20a%20pound%20of%20bacon"));
 
   dump(userdata2, strlen(userdata2));
-  //length = aes_encrypt_cbc(plaintext, ciphertext, strlen(plaintext), key, iv, 16);
   length = aes_encrypt_cbc(userdata2, ciphertext, strlen(userdata2), key, iv, 16);
 }
 
@@ -48,10 +50,6 @@ int is_admin(char *ptr, int length)
   plength = aes_decrypt_cbc(decrypted, ptr, length, key, iv, 16);
   dump(decrypted, plength);
 
-  //i = strcmp(decrypted, (char *)";admin=true;");
-  //i = strcmp(decrypted, (char *)"TESTI");
-  //i = strcmp((char *)"TESTIx", decrypted);
-  //i = strstr(decrypted, (char *)"TESTIx");
   if(strstr(decrypted, (char *)";admin=true;")==NULL)
   {
     printf("is_admin(): you are not admin\n");
@@ -66,14 +64,14 @@ int is_admin(char *ptr, int length)
 
 int main(void)
 {
-  //char plaintext[] = "YELLOW";
-  char plaintext[] = "YELLOW0123456789abcdef0123456789abcdef0123456789";
   char
     userdata[1024];
-  int length;
+  int
+    length,
+    ret = 1;
 
   randomize_key(key, 16);
-  randomize_key(iv, 16);
+  randomize_key(iv,  16);
 
   memset(userdata, 0, sizeof(userdata));
   strncpy(userdata, ";admin=true", strlen(";admin=true"));
@@ -82,47 +80,15 @@ int main(void)
 
   strncpy(userdata, "XadminXtrue", strlen(";admin=true"));
   printf("adversary: trying \"%s\":\n", userdata);
-  //memcpy(userdata, "XadminXtrueX", strlen(";admin=true;"));
   length = first_function(userdata, ciphertext);
   *(ciphertext+16) ^= 'X' ^ ';';
   *(ciphertext+22) ^= 'X' ^ '=';
   printf("\n\nciphertext:\n");
   dump(ciphertext, length);
-  //length = first_function((char *)"TESTI", ciphertext);
 
-  if(is_admin(ciphertext, length))
+  if(is_admin(ciphertext, length)) {
     printf("great success!\n");
-
-  /*
-  print_bits('A');
-  printf("\n");
-  print_bits('B');
-  printf("\n");
-  print_bits(0x41);
-  print_bits(0x41 ^ (1 << 4));
-  print_bits(0x41 ^ (1 << 3));
-  print_bits(0x41 ^ (1 << 2));
-  print_bits(0x41 ^ (1 << 1));
-  print_bits(0x41 ^ (1 << 0));
-  //return;
-  printf("\n\n");
-
-  printf("plaintext=%s\n", plaintext);
-  length = aes_encrypt_cbc(plaintext, ciphertext, strlen(plaintext), key, iv, 16);
-  printf("length=%d\n", length);
-
-  printf("\ndecrypted:\n");
-  aes_decrypt_cbc(decrypted, ciphertext, length, key, iv, 16);
-  //aes_decrypt(decrypted, ciphertext, length, key, 16);
-  dump(decrypted, length);
-
-  // modify one bit
-  *(ciphertext+5) = *(ciphertext+5) ^ ( 1 << 5 );
-  aes_decrypt_cbc(decrypted, ciphertext, length, key, iv, 16);
-  dump(decrypted, length);
-
-
-  printf("\n\n");
-  first_function("TESTI", ciphertext);
-  */
+    ret = 0;
+  }
+  return ret;
 }
