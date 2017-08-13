a027c14f12ae9105c9f3b24d7b4d905e956b2d0c
pyllyukko <pyllyukko@maimed.org>
2017-08-13 16:57:28 +0300

    Removed i var from 15.c

--- 2-15-padding/15.c
+++ 2-15-padding/15.c
@@ -4,19 +4,18 @@
 int main(void)
 {
   char plaintext[] = "ICE ICE BABY\x04\x04\x04\x04"; // valid padding
-  int i;
 
   printf("PKCS#7 padding validation\n\nvalid case:\n");
-  i = padding_detect(plaintext, strlen(plaintext), 20);
+  padding_detect(plaintext, strlen(plaintext), 20);
   printf("\ntrying with \"\\x05\\x05\\x05\\x05\"\n");
-  i = padding_detect((char *)"ICE ICE BABY\x05\x05\x05\x05", strlen("ICE ICE BABY\x05\x05\x05\x05"), 20); // invalid padding
+  padding_detect((char *)"ICE ICE BABY\x05\x05\x05\x05", strlen("ICE ICE BABY\x05\x05\x05\x05"), 20); // invalid padding
   printf("\ntrying with \"\\x01\\x02\\x03\\x04\"\n");
-  i = padding_detect((char *)"ICE ICE BABY\x01\x02\x03\x04", strlen("ICE ICE BABY\x01\x02\x03\x04"), 20); // invalid padding
+  padding_detect((char *)"ICE ICE BABY\x01\x02\x03\x04", strlen("ICE ICE BABY\x01\x02\x03\x04"), 20); // invalid padding
   printf("\ntrying with no padding\n");
-  i = padding_detect((char *)"ICE ICE BABY", strlen("ICE ICE BABY"), 20); // no padding
+  padding_detect((char *)"ICE ICE BABY", strlen("ICE ICE BABY"), 20); // no padding
 
   printf("\nstring = %s\n", "ICE ICE BABY1234");
-  i = padding_detect((char *)"ICE ICE BABY1234", strlen("ICE ICE BABY1234"), 20); // invalid padding
+  padding_detect((char *)"ICE ICE BABY1234", strlen("ICE ICE BABY1234"), 20); // invalid padding
 
   return 0;
 }
